2d27bb47dbdc43809ed9c9b746997ec865cb8f4f
Gustavo da Cruz <161187263+GustavoCruz9@users.noreply.github.com>
2024-02-26 00:15:30 -0300

    first commit

---
+++ CrudJavaWeb/src/main/java/model/Disciplina.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Disciplina {
+
+	private int codigo;
+	private String nome;
+	private Professor professor;
+	
+}
---
+++ CrudJavaWeb/src/main/java/model/Professor.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Professor {
+
+	private int codigo;
+	private String nome;
+	private String titulacao;
+	
+}
